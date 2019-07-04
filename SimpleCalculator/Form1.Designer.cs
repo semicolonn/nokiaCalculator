7e3628697a500641449c2df5149d4a00d7858813
baed <edrisebaher@gmail.com>
2019-07-04 10:58:08 +0430

    nokia calculator

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Addition = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Addition
+            // 
+            this.Addition.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addition.Location = new System.Drawing.Point(33, 195);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(100, 53);
+            this.Addition.TabIndex = 0;
+            this.Addition.Text = "Add";
+            this.Addition.UseVisualStyleBackColor = true;
+            this.Addition.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(33, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 33);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(163, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Minus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(163, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 53);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(33, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 53);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Divid";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(33, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 33);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox3.Location = new System.Drawing.Point(33, 140);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(242, 33);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(33, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(313, 406);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Addition);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Nokia Calc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.ReadOnly = true;
+            textBox1.Text = "Enter first number";
+            textBox2.Text = "Enter second number";
+            Addition.Text = "+";
+            button2.Text = "-"; 
+            button3.Text = "*"; 
+            button4.Text = "/";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+                int result = 0;
+
+                a = Convert.ToInt16(textBox1.Text);
+                b = Convert.ToInt16(textBox2.Text);
+                result = a + b;
+                textBox3.Text = result.ToString();
+                //MessageBox.Show("Addition Operation is:" + " " +result.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Please enter numeric value(s)!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int a = 0;
+                int b = 0;
+                int result = 0;
+
+                a = Convert.ToInt16(textBox1.Text);
+                b = Convert.ToInt16(textBox2.Text);
+                result = a - b;
+                textBox3.Text = result.ToString();
+                //MessageBox.Show("Minus Operation is:" + " " + result.ToString());
+            }
+            catch {
+                MessageBox.Show("Please enter numeric value(s)!");
+            
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+                int result = 0;
+
+                a = Convert.ToInt16(textBox1.Text);
+                b = Convert.ToInt16(textBox2.Text);
+                result = a / b;
+                textBox3.Text = result.ToString();
+                //MessageBox.Show("Divid Operation is:" + " " + result.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Please enter numeric value(s)!");
+
+            }
+        }
+          
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+                int result = 0;
+
+                a = Convert.ToInt16(textBox1.Text);
+                b = Convert.ToInt16(textBox2.Text);
+                result = a * b;
+
+                textBox3.Text = result.ToString();
+                //MessageBox.Show("Multiply Operation is:" + " " + result.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Please enter numeric value(s)!");
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Enter first number" && textBox2.Text == "Enter second number")
+            {
+                return;
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
